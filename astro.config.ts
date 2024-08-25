9abae5a73704fadbf3b0b26f6307a9fea3152c30
krarpit <kumar.arpit77@yahoo.com>
2024-08-25 02:15:44 -0700

    complete tail calls article

--- astro.config.ts
+++ astro.config.ts
@@ -11,62 +11,62 @@ import rehypeKatex from "rehype-katex";
 import racketGrammar from "./src/assets/syntaxes/racket.tmLanguage.json";
 
 const racket = {
-	id: "Racket",
-	scopeName: "source.racket",
-	grammar: racketGrammar,
-	aliases: ["rkt", "racket"],
+  id: "Racket",
+  scopeName: "source.racket",
+  grammar: racketGrammar,
+  aliases: ["rkt", "racket"],
 };
 
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://kumarpit.github.io",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkMath],
-		rehypePlugins: [
-			[rehypeExternalLinks, { target: "_blank", rel: ["nofollow, noopener, noreferrer"] }],
-			[rehypeKatex, {}],
-		],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "slack-dark",
-			wrap: true,
-			langs: [racket],
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-	],
-	image: {
-		domains: ["webmention.io"],
-	},
-	// https://docs.astro.build/en/guides/prefetch/
-	prefetch: true,
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://kumarpit.github.io",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkMath],
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["nofollow, noopener, noreferrer"] }],
+      [rehypeKatex, {}],
+    ],
+    remarkRehype: { footnoteLabelProperties: { className: [""] } },
+    shikiConfig: {
+      theme: "slack-dark",
+      wrap: true,
+      langs: ['c', racket],
+    },
+  },
+  integrations: [
+    mdx({}),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+  image: {
+    domains: ["webmention.io"],
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
 
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
 }
